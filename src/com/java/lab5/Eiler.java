fb5508b00732031a93e3715adb4c032fbd81d2ad
Oleksii Tkachuk <vaporstudy878@gmail.com>
2021-05-14 00:02:26 +0300

    for report

--- src/com/java/lab5/Eiler.java
+++ src/com/java/lab5/Eiler.java
@@ -8,7 +8,7 @@ public class Eiler {
     private double h = 0.1, start = 0;
     private double a = 0, b = 2;
     private double x = a, y = start;
-    public void eilerStraight() {
+    public void eilerStraight() {//Ейлера прямий
         while (x < b) {
             y = y + h * f(x + (h / 2), y + (h / 2 * f(x, y)));
             x += h;
@@ -16,7 +16,7 @@ public class Eiler {
         }
     }
 
-    public void eilerReverse(double x, double b, double h, double y){
+    public void eilerReverse(double x, double b, double h, double y){//Ейлера зворотній
         double d, c, xd = 0, yd = 1, yd1 = 0, hd = h/2;
         for (; x < b; x += h){
             y += h * f(x, y );
@@ -27,10 +27,11 @@ public class Eiler {
             }
             c = (yd1 - y) / (h * h - h * h / 4);
             d = c * h * h;
-            System.out.println("x = " + x + " | y = " + y + " | yd = " + yd1 + " | c =  " + d);
+            System.out.println("x = " + x + " \ny = " + y + " \nyd = " + yd1 + " \nc =  " + d);
+            System.out.println("---------------------------------------------");
         }
     }
-    public void eilerCorrected(){
+    public void eilerCorrected(){//Ейлера виправлений
         while (x < b){
             y = y + (h/2) * Math.abs(f(x, y) + f(x + h, y + h * f(x, y)));
             x += h;
